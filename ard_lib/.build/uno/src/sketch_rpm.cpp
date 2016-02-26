d76083f3cf3ce16c05b794056c334d6a26f018b8
stelmach ro <stelmach.ro@gmail.com>
2016-02-27 00:56:23 +0200

    arduino sketch minor fixes

--- ard_lib/.build/uno/src/sketch_rpm.cpp
+++ ard_lib/.build/uno/src/sketch_rpm.cpp
@@ -2,27 +2,89 @@
 
 void setup();
 void loop();
+void LedsMagic(int value);
 #line 1 "src/sketch_rpm.ino"
+const int delayValue = 25;
+
 int led1 = 2;
 int led2 = 4;
 int led3 = 7;
 int led4 = 8;
 
-int counter = 0;
+void LedsMagic(int);
 
 void setup() {
   Serial.begin(9600);
+
+	pinMode(led1,OUTPUT);
+	pinMode(led2,OUTPUT);
+	pinMode(led3,OUTPUT);
+	pinMode(led4,OUTPUT);
 }
 
 void loop() {
-  
-	 counter++;
-		delay(20);
-	 if ( counter == 10000) {
-		 Serial.println(counter);
-		 counter = 0;
-	 }
+	//keep the delay's methods as they are
+  delay(delayValue);
+  int sensorvalue = analogRead(A0);
+  Serial.println(sensorvalue);
+  delay(delayValue); 
 
-	 delay(20);
-
+	LedsMagic(sensorvalue);
 }
+
+void LedsMagic(int value) {
+	
+	int tmpdelay = 0;
+
+	if (value > 0 && value <= 250) 
+		tmpdelay = 1000;
+	else if (value >= 250 && value <= 500)
+		tmpdelay = 500;
+	else if (value >= 500 && value <= 750)
+		tmpdelay = 250;
+	else
+		tmpdelay = 100;
+
+		digitalWrite(led1,HIGH);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,HIGH);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,HIGH);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,HIGH);
+		delay(tmpdelay);
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ard_lib/src/sketch_rpm.ino
+++ ard_lib/src/sketch_rpm.ino
@@ -1,48 +1,84 @@
+const int delayValue = 25;
+
 int led1 = 2;
 int led2 = 4;
 int led3 = 7;
 int led4 = 8;
 
+void LedsMagic(int);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+
+	pinMode(led1,OUTPUT);
+	pinMode(led2,OUTPUT);
+	pinMode(led3,OUTPUT);
+	pinMode(led4,OUTPUT);
 }
 
 void loop() {
-  
+	//keep the delay's methods as they are
+  delay(delayValue);
   int sensorvalue = analogRead(A0);
   Serial.println(sensorvalue);
-  
-  
-  if (sensorvalue >= 0 && sensorvalue < 200) {
-    digitalWrite(led1,HIGH);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,LOW);
-    digitalWrite(led4,LOW);
-  }
- else if (sensorvalue >= 200 && sensorvalue < 400) {
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,HIGH);
-    digitalWrite(led3,LOW);
-    digitalWrite(led4,LOW);
-  } 
-  else if (sensorvalue >= 400 && sensorvalue < 800) {
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,HIGH);
-    digitalWrite(led4,LOW);
-  }
-  else {
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,LOW);
-    digitalWrite(led4,HIGH);
-  }
-  
-  delay(300);
-  
+  delay(delayValue); 
+
+	LedsMagic(sensorvalue);
 }
+
+void LedsMagic(int value) {
+	
+	int tmpdelay = 0;
+
+	if (value > 0 && value <= 250) 
+		tmpdelay = 1000;
+	else if (value >= 250 && value <= 500)
+		tmpdelay = 500;
+	else if (value >= 500 && value <= 750)
+		tmpdelay = 250;
+	else
+		tmpdelay = 100;
+
+		digitalWrite(led1,HIGH);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,HIGH);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,HIGH);
+		digitalWrite(led4,LOW);
+		delay(tmpdelay);
+		digitalWrite(led1,LOW);
+		digitalWrite(led2,LOW);
+		digitalWrite(led3,LOW);
+		digitalWrite(led4,HIGH);
+		delay(tmpdelay);
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
