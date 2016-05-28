9ed774cb1ea8438fcd16a36ede41463ee5eb1565
stelro <stelmach.ro@gmail.com>
2016-05-28 22:33:34 +0300

    gnu license

--- rpm/main.cpp
+++ rpm/main.cpp
@@ -1,3 +1,26 @@
+/*
+ *  main.cpp - Main functions, caller for QApplication
+ *
+ *  This file is a part of RPM Counter
+ *  Copyright (C) 2016 KASTORIA Robotics Team
+ *  Main Author, Stelmach Rostislav, stelmach.ro<at>gmail.com
+ *  Main Contributors, Michail Alexandros Savvanis, michalsavv<at>gmail.com
+ *  and Perlat Kociaj, perlatkotsiai<at>outlook.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "arduinorpm.h"
 #include "splashdialog.h"
 #include <QApplication>
--- rpm/splashdialog.cpp
+++ rpm/splashdialog.cpp
@@ -1,3 +1,27 @@
+/*
+ *  splashdialog.cpp - the first dialog shown with the start of the application
+ *  used for selecting the available serila's port's by the user
+ *
+ *  This file is a part of RPM Counter
+ *  Copyright (C) 2016 KASTORIA Robotics Team
+ *  Main Author, Stelmach Rostislav, stelmach.ro<at>gmail.com
+ *  Main Contributors, Michail Alexandros Savvanis, michalsavv<at>gmail.com
+ *  and Perlat Kociaj, perlatkotsiai<at>outlook.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "splashdialog.h"
 #include "ui_splashdialog.h"
 
